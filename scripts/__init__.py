558ede57e4ff3c60f55e870ef1c53f73f6dadcb4
laserpointlabs <laserpointlabs@gmail.com>
2025-06-25 10:16:11 -0400

    feat: Add version tracking in scripts/__init__.py for v0.1.0

---
+++ scripts/__init__.py
@@ -0,0 +1,7 @@
+"""
+Universal Time Tracker Scripts Package
+"""
+
+__version__ = "0.1.0"
+__author__ = "Universal Time Tracker Team"
+__description__ = "Scripts and utilities for Universal Time Tracker" 
